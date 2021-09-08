d20ac9ae629d76bd1199bd163955b047943cdd09
Edward Cluster <ecluster@csumb.edu>
2021-09-08 11:46:01 -0700

    Initialized unit tests for MainActivity methods

---
+++ app/src/main/java/com/example/cst438proj01/JsonPlaceHolderAPI.java
@@ -0,0 +1,30 @@
+package com.example.cst438proj01;
+
+import android.telecom.Call;
+
+import androidx.room.Query;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
+
+public class JsonPlaceHolderAPI {
+
+
+
+}
