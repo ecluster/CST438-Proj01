bdad93412bbeb3f099e2b0539d4e912317de80be
Dylan Hill <dylanrayhill@msn.com>
2021-09-15 12:05:03 -0700

    instrumented test pass

--- app/src/main/java/com/example/cst438proj01/CreateAcc.java
+++ app/src/main/java/com/example/cst438proj01/CreateAcc.java
@@ -1,16 +1,13 @@
 package com.example.cst438proj01;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 public class CreateAcc extends AppCompatActivity implements View.OnClickListener {
     View btnCreatAcc;
